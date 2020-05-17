09a00d99fe5933714f71a792d300a63ea4b8aaf8
Tom Zheng <zsy.tom93@gmail.com>
2020-05-17 16:40:57 -0400

    trying to make a parsing program for expressions

--- test_parse.cpp
+++ test_parse.cpp
@@ -1,15 +1,29 @@
 #include <string.h>
+#include <iostream>
 using namespace std;
 
-class Term {
+class Expr {
    public:
-      string exp;   // Length of a box
-      string ;  // Breadth of a box
-      double height;   // Height of a box
+      string expr;   // expression of the term in string
+      Expr* terms ;  // list of terms in this term, NULL if this is the last level
+      Expr(string expr):expr(expr){
+        this->Set();
+      };
+      void Set(){
+        this->terms = NULL;
+      };
+
 };
 
 int main(){
   string test_string = "5/(x-3) + sin(45*log(2n -2))";
+  cout<< test_string; cout<<"\n";
+  Expr new_expr(test_string);
+  cout <<new_expr.expr; cout<<"\n";
+  if (new_expr.terms == NULL){
+    cout<< "true";
+  }
+  return 0;
 
 
 }
