3a1c6d34691ae41d3bd09474e5506323bbb6b05a
Tom Zheng <zsy.tom93@gmail.com>
2020-05-17 17:37:24 -0400

    added space removal

--- test_parse.cpp
+++ test_parse.cpp
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <iostream>
+#include <cctype>
 using namespace std;
 
 class Expr {
@@ -12,14 +13,31 @@ class Expr {
       void Set(){
         this->terms = NULL;
       };
+      void s1(){
+        //search for brackets first
+        for(int i=0;i<expr.size();i++){
+        }
+      };
 
 };
+
+string str_no_space(string str){
+  for(string::iterator i=str.begin();i!=str.end();i++){
+    while (i!=str.end()&& isspace(*i)){
+      str.erase(i);
+    }
+  }
+  return str;
+}
 
 int main(){
   string test_string = "5/(x-3) + sin(45*log(2n -2))";
   cout<< test_string; cout<<"\n";
-  Expr new_expr(test_string);
+  string new_string = str_no_space(test_string);
+  
+  Expr new_expr(new_string);
   cout <<new_expr.expr; cout<<"\n";
+
   if (new_expr.terms == NULL){
     cout<< "true";
   }
