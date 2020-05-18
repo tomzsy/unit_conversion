e49d9558fb0c56ac0fc0d8e758af28b714473205
Tom Zheng <zsy.tom93@gmail.com>
2020-05-18 16:33:22 -0400

    changed term search behavior

--- test_parse.cpp
+++ test_parse.cpp
@@ -4,35 +4,84 @@
 #include <stdexcept>
 #include <vector>
 #include <algorithm>
+#define TYP_T 1 //term type
+#define TYP_O 2 //operator type
+
 using namespace std;
 
-const string all_operators = "!,+-*/^"
+const string all_operators = "!,+-*/^";
 
 class Expr {
    public:
-      string::iterator start;   // start and end of the string
-      string::iterator end;
-      vector<Expr> terms ;  // list of terms in this term, NULL if this is the last level
+      int start;   // start and end of the string
+      int end;
+      string expr;
+      string fn = "()"; //default type is a term
+      vector<Expr> terms ;  // list of terms in this term
       //constructors
-      Expr(string &expr){
-        start = expr.begin();
-        end = expr.end();
+      Expr(string input_expr):expr(input_expr){
+        start = 0;
+        end = expr.end()-expr.begin();
+        this->Set();
+      }; //root expression
+      Expr(string input_expr, int start, int end): // bracket sub-expression
+      expr(input_expr),start(start), end(end){
         this->Set();
       };
-      Expr(string::iterator start, string::iterator end): start(start), end(end){
-        this->Set();
-      };
+      Expr(string input_expr, int start, int end, bool is_term):
+      start(start), end(end){
+        if(is_term)
+        expr = input_expr;
+        else
+        fn = input_expr;
+      }; //No need to set, is only a single term or a operator
+
       //methods
       void Set(){
         this->search1();
       };
       void search1(){
-        //search for brackets first
+        //search for terms and brackets
         int cur_b_lvl = 0; //current bracket level
-        string::iterator start_loc;
-        string::iterator end_loc;
-        //cout<<string(start,end)<<"\n";
-        for(string::iterator it = start; it!=end; it++){
+        string::iterator start_loc;//start location of brackets
+        string::iterator end_loc;//end location of brackets
+        bool is_term =false;// is currently at a "term"
+        string::iterator term_start;//start of term
+        string::iterator term_end;
+        for(string::iterator it = expr.begin(); it!=expr.end(); it++){
+          switch (*it){
+            case ')':
+            break;
+            case '!':
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '^':
+            if(cur_b_lvl == 0){
+              if(is_term){
+                //there is a term beforehand
+                term_end = it;
+                terms.push_back(Expr(string(term_start,term_end),term_start-expr.begin(), term_end-expr.begin(),true));
+                is_term = false;
+              }
+              terms.push_back(Expr( string(1,*it),it-expr.begin(), it-expr.begin()+1,false));
+            }
+            break;
+            case '(':
+            if(cur_b_lvl == 0&& is_term){
+                //there is a term beforehand
+                term_end = it;
+                terms.push_back(Expr(string(term_start,term_end),term_start-expr.begin(), term_end-expr.begin(),true));
+                is_term = false;
+            }
+            break;
+            default:
+            if(cur_b_lvl == 0 && is_term ==false){
+            is_term = true;
+            term_start = it;
+            }
+          };
           if((*it)=='('){
             if ( cur_b_lvl==0)
               start_loc = it +1;
@@ -46,18 +95,18 @@ class Expr {
               end_loc = it;
               if (end_loc<start_loc)
               throw std::invalid_argument( "empty brackets" );
-              terms.push_back(Expr(start_loc,end_loc));
+              terms.push_back(Expr(string(start_loc,end_loc),start_loc-expr.begin()-1, end_loc-expr.begin()+1));
             }
             cur_b_lvl--;
           }
+
         }
       };
-
-      bool search2(){
+/*
+      void search2(){
         //search for term term
-        string::iterator it = start
+        string::iterator it = start;
         vector<Expr>::iterator it_vec = this->terms.begin();
-        bool is_term =false;
         string::iterator it_term ;
         for(; it!= end;){
           if(it == it_vec->start-1){
@@ -67,12 +116,14 @@ class Expr {
         }
         all_operators.find()
       }
+      */
 };
 
 
 
 void disp_content(Expr expr){
-  cout<<"This Content:" <<string(expr.start, expr.end)<<"\n";
+  cout<<"This Content:" <<expr.expr <<"\n";
+  cout<<"This Operator" <<expr.fn <<"\n";
   cout<<"Sub Contents"<<"\n";
   if (expr.terms.empty()){
     cout<<"none\n";
